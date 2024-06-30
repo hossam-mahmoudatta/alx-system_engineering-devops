88ea4e58633c26fa896afd3ce565be11a1be9cf2
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-07-01 00:06:46 +0300

    trying a code for task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Installs flask v2.1.0 using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# package { 'flask':
+#   ensure   => '2.1.0',
+#   provider => 'pip3'
+# }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin']
 }
