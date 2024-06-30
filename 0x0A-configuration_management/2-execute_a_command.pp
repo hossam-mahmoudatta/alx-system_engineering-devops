8954ba9dfd54d0d2f259fa5ebdfab4fd5f1c4ae9
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-30 23:53:01 +0300

    edited task-2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # creates a script that kills
 # The process named killmenow
 
-exec { 'pkill':
-  command    => 'pkill killmenow',
-  provider   => 'shell'
+exec { 'kill killmenow process':
+  command => 'pkill killmenow',
+  path    => '/usr/bin/:/bin'
 }
